e0b43a1f54b3d37d6578ca755e728937be14d86c
19020440 <you@example.com>
2021-12-29 11:31:25 +0700

    sua lai cai vua roi merge bi sai

--- src/pages/Question/BodyQuestion.jsx
+++ src/pages/Question/BodyQuestion.jsx
@@ -22,7 +22,7 @@ function BodyQuestion({ questions, onCloseModal, onBackToPrev }) {
     // sessionStorage.setItem('arrIndexQuestion', `[${arrIndexQuestion.toString()}]`);
     // console.log('set lai index');
 
-    const [typeQuestion, setTypeQuestion] = useState(questions[6]);
+    const [typeQuestion, setTypeQuestion] = useState(questions[0]);
 
     const [refElMain] = useGlobalState("elMain");
     useEffect(() => {
