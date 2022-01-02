b86d532346f57601395e3a7ebe3d04341a0c12d2
19020440 <you@example.com>
2022-01-02 13:12:13 +0700

    fix-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 // import './App.css';
 import 'animate.css';
 import routes from './pages/routes';
@@ -9,6 +9,10 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
+        <Route
+          path="/mobile-ui"
+          element={<Navigate to="/" />}
+        />
           {
             routes.map(({ path, element }, index) => {
               return (
